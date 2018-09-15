cd738400f712c83c125efadebdcbe6c4e7f6e965
Nguyen <nguyenthonguyen.uit@gmail.com>
2018-09-15 23:07:05 +0700

    Implement ManageInit to test connection

--- app/src/main/java/com/demo/pos/poslinkdemo/presenters/IBasePresenter.kt
+++ app/src/main/java/com/demo/pos/poslinkdemo/presenters/IBasePresenter.kt
@@ -2,13 +2,29 @@ package com.demo.pos.poslinkdemo.presenters
 
 import android.content.Context
 import com.pax.poslink.PosLink
+import com.pax.poslink.ProcessTransResult
 import com.pax.poslink.poslink.POSLinkCreator
+import io.reactivex.Observable
 
 abstract class IBasePresenter<V>(protected var view: V) where V : IBaseView {
 
     protected fun createPosLinkObject(): PosLink {
         return POSLinkCreator.createPoslink(getContext())
     }
+
+    protected fun createPosLinkObservable(): Observable<PosLink> {
+        return Observable.create<PosLink>{
+            it.onNext(POSLinkCreator.createPoslink(getContext()))
+        }
+    }
+
+    /**
+     * Execute the posLink request and return result set in an observable
+     */
+    protected fun executePosLink(posLink: PosLink): ProcessTransResult {
+        return posLink.ProcessTrans()
+    }
+
 
     private fun getContext(): Context {
         return view.getContext()
--- app/src/main/java/com/demo/pos/poslinkdemo/presenters/manage/ManagePresenterImpl.kt
+++ app/src/main/java/com/demo/pos/poslinkdemo/presenters/manage/ManagePresenterImpl.kt
@@ -3,11 +3,21 @@ package com.demo.pos.poslinkdemo.presenters.manage
 import com.demo.pos.poslinkdemo.configuration.PosLinkConfiguration
 import com.demo.pos.poslinkdemo.tasks.ManageTask
 import com.pax.poslink.ManageRequest
+import com.pax.poslink.PosLink
+import io.reactivex.functions.BiFunction
 
 class ManagePresenterImpl(view: IManagePresenter.IManageBaseView) : IManagePresenter.Presenter(view) {
 
     override fun callManageInitRequest() {
         ManageTask.createManageRequestObservable(PosLinkConfiguration.TRAN_TYPE_MANAGE_INIT)
+                .zipWith(createPosLinkObservable(), BiFunction { manageRequest: ManageRequest, posLink: PosLink ->
+                    posLink.ManageRequest = manageRequest
+                    executePosLink(posLink)
+                }).subscribe({
+
+                }, {
+
+                })
     }
 
 }
